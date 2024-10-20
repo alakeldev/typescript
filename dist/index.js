fce92dfe1a4e7aca78c596a2aa7e5a4525b168d2
Abdullah Alakel <abdullah.k.alakel@gmail.com>
2024-10-20 13:59:50 +0200

    edit files ts and js

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,17 @@
 "use strict";
-function getEasySeconds() {
-    return 3;
+class User {
+    static getCount() {
+        console.log(`${this.created} Objects Created`);
+    }
+    constructor(username) {
+        this.username = username;
+        User.created++;
+    }
 }
-var Kids;
-(function (Kids) {
-    Kids[Kids["Five"] = 25] = "Five";
-    Kids[Kids["Seven"] = 20] = "Seven";
-    Kids[Kids["Ten"] = 15] = "Ten";
-})(Kids || (Kids = {}));
-var Level;
-(function (Level) {
-    Level[Level["Kid"] = 15] = "Kid";
-    Level[Level["Easy"] = getEasySeconds()] = "Easy";
-    Level[Level["Medium"] = Level.Easy] = "Medium";
-    Level[Level["Hard"] = Level.Medium + 5] = "Hard";
-})(Level || (Level = {}));
-let lvl = "Easy";
-if (lvl === "Easy") {
-    console.log(`The Level is ${lvl} and Number of Seconds is ${Level.Easy}`);
-}
+User.created = 0;
+let u1 = new User("Alakel");
+let u2 = new User("Abode");
+let u3 = new User("Abd");
+let u4 = new User("Mama");
+User.getCount();
 //# sourceMappingURL=index.js.map
